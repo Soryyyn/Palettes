9483f63430c4c40b94dea958927e7c2380067ead
Soryyyn <soryn.baechli@gmail.com>
2021-01-19 20:35:50 +0100

    ✨ added basic preact & snowpack setup

---
+++ client/snowpack.config.js
@@ -0,0 +1,36 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: { url: '/', static: true },
+    src: { url: '/dist' },
+  },
+  plugins: [
+    '@snowpack/plugin-dotenv',
+    '@snowpack/plugin-typescript',
+    '@prefresh/snowpack',
+    ["@snowpack/plugin-sass", {
+      style: "compressed"
+    }]
+  ],
+  routes: [
+    /* Enable an SPA Fallback in development: */
+    { "match": "routes", "src": ".*", "dest": "/index.html" },
+  ],
+  optimize: {
+    /* Example: Bundle your final build: */
+    "bundle": true,
+  },
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  alias: {
+    "react": "preact/compat",
+    "@app": "./src"
+  }
+};
